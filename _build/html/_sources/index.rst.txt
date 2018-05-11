1e579d0a6dac527a8055784423fa1a68bec7e0d6
Zimoune <lepallac.simon@gmail.com>
2018-05-11 16:50:35 +0200

    welcome

--- _build/html/_sources/index.rst.txt
+++ _build/html/_sources/index.rst.txt
@@ -3,7 +3,7 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to Alydoc's documentation!
+Bienvenue sur le guide d'Alykraft
 ==================================
 
 .. toctree::
@@ -12,7 +12,7 @@ Welcome to Alydoc's documentation!
 
 
 
-Indices and tables
+Sommaire
 ==================
 
 * :ref:`genindex`
--- _build/html/searchindex.js
+++ _build/html/searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,pag:0,recherch:0},titles:["Welcome to Alydoc\u2019s documentation!"],titleterms:{alydoc:0,and:0,document:0,indic:0,tabl:0,welcom:0}})
+Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,pag:0,recherch:0},titles:["Bienvenue sur le guide d\u2019Alykraft"],titleterms:{alydoc:[],alykraft:0,and:[],bienvenu:0,document:[],guid:0,indic:[],sommair:0,tabl:[],welcom:[]}})
